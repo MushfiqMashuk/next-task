fdfdc3feef244cb1b1c7c8a5b18e74e8f223f26a
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-10-03 12:29:40 +0600

    adding debounce to the search

--- components/Body/index.jsx
+++ components/Body/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Form from "../Form";
 import Modal from "../Modal";
 import UserCard from "../UserCard";
@@ -7,12 +7,34 @@ import styles from "./body.module.scss";
 const Body = () => {
   const [users, setUsers] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [searchValue, setSearchValue] = useState(null);
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+        func.apply(context, args);
+      }, 500);
+    };
+  };
+
+  const handleChange = (value) => {
+    setSearchValue(value);
+  };
+
+  const optimizedFn = useCallback(debounce(handleChange), []);
 
   useEffect(() => {
     const fetchUser = async () => {
       const data = await fetch(process.env.NEXT_PUBLIC_USERS_API);
 
       const userData = await data.json();
+
+      // set the user state
       setUsers(userData);
     };
 
@@ -23,7 +45,12 @@ const Body = () => {
   return (
     <div className={styles.container}>
       <div className={styles.head}>
-        <input type="text" placeholder="search" className={styles.search_bar} />
+        <input
+          type="text"
+          placeholder="search"
+          className={styles.search_bar}
+          onChange={(e) => optimizedFn(e.target.value)}
+        />
         <button
           className={styles.add_button}
           onClick={() => setShowModal(true)}
